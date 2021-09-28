f1add26a9cf0312c8cf9ad045f8a86451c06d23a
Estrada Bastida Porfirio <al222011231@gmail.com>
2021-09-28 01:51:39 -0500

    Creacion de rutas

--- routes/web.php
+++ routes/web.php
@@ -9,30 +9,58 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+
+/* RUTA DESDE FUNCTION */
+Route::get('/route', function () {
+    return "soy un hola mundo desde una ruta";
 });
 
-/* RUTA PRINCIPAL */
+/* RUTA DESDE FUNCTION */
+Route::get('/funciones', function () {
+    return "HOLA MUNDO";
+});
+
+/* CARPETA view/productos/product.blade */
+Route::get('/view/productos/product.blade', function () {
+    return view('producto/produc');
+});
+
+/* RUTA DE BLADE */
 Route::get('/', function () {
     return view('mipag');
 });
 
-/* RUTA DE PRODUCTOS */
+
+
+
+/*
+ RUTA PRINCIPAL 
+Route::get('/', function () {
+    return view('mipag');
+});
+ RUTA DE PRODUCTOS 
 Route::post('/productos', function () {
     return view('producto/produc');
 });
-/* RUTA DESDE FUNTION */
-Route::put('/funrut', function () {
+ RUTA DESDE FUNTION 
+Route::get('/funrut', function () {
     return "soy un hola mundo desde una ruta";
 });
-/* RUTA DE BLADE */
-/*Route::get('/miprimerl', function () {
-    return view('child')->name('mi primer layout');
-});*/
+ RUTA DE BLADE 
+Route::get('/producto', function () {
+    return view('producto/produc');
+});
+*/
 
+/* RUTA DE PRODUCTOS */
+Route::post('/productos', function () {
+    return view('producto/produc');
+    
+});
+
+/*
 Route::delete('miprimerl', function () {
     return "soy una ruta con mame";
 })->name('layout');
+*/
